6657ddda1db6bf3c5accf74945e2cf6b31ba7edb
dawirashid <77828860+dawirashid@users.noreply.github.com>
2022-02-02 22:10:28 +0800

    add facebook_auth

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -1,6 +1,7 @@
 import 'package:facebooksignin_button/profilepage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -20,11 +21,16 @@ class _HomepageState extends State<Homepage> {
             child: SignInButton(
               Buttons.FacebookNew,
               text: 'Continue with Facebook',
-              onPressed: () {
+              onPressed: () async {
+                final result = await FacebookAuth.i
+                    .login(permissions: ['email', 'public_profile']);
+                if (result.status == LoginStatus.success) {
+                  setState(() {});
+                }
                 Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Profilepage()),
-                );
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const Profilepage()));
               },
             ),
           ),
--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class Profilepage extends StatefulWidget {
   const Profilepage({Key? key}) : super(key: key);
@@ -13,7 +14,9 @@ class _ProfilepageState extends State<Profilepage> {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: (){
+          onPressed: () async {
+            await FacebookAuth.i.logOut();
+            setState(() {});
             Navigator.pop(context);
           },
           child: const Text('sign out'),
